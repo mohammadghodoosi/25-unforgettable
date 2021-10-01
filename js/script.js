ca6c24a99bcb24071a99659fcf3f963ea19d46d8
Mohammad Ghodoosi <ghodusimohamad@gmail.com>
2021-10-01 13:13:15 +0330

    second commit
    showing result in ui in the form of cards

--- js/script.js
+++ js/script.js
@@ -7,7 +7,15 @@ class Data{
   }
 }
 //showing data in UI
-
+class UI{
+  constructor(data){
+    this.data=data
+  }
+  showInField(data){
+    const result=document.getElementById('result')
+    result.innerHTML+=`<div class='card mx-auto' style='width: 18rem'><div class='card-body'><h5 class='card-title'>${data.task}</h5><p class='card-text'>start time:  ${data.start} <br> ending time:  ${data.end}</p><a href='#' class='card-link'>Done</a><a href='#' class='card-link'>edit</a></div></div>`
+  }
+}
 //Iterating data
 
 //resisting data
@@ -16,12 +24,17 @@ class Data{
 
 //code pool
 const btnSubmit=document.getElementById('btnSubmit')
-btnSubmit.addEventListener('click',(e)=>{
+const text=document.getElementById('text');
+    text.innerHTML="<h1 class='col d-flex justify-content-center text-center'>no tasks yet...<br>please insert your daily tasks</h1>"
+btnSubmit.addEventListener('click',()=>{
   const task=document.getElementById('task').value
   const start=document.getElementById('start').value
   const end=document.getElementById('end').value
   if(task!=''&start!=''&end!=''){
     const data=new Data(task,start,end)
+    text.innerHTML=''
+    const ui=new UI
+    ui.showInField(data)
   }else{
     alert('fill all the field please...')
   }
